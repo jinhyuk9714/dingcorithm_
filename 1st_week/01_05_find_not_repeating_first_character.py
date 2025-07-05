1250217acec96ac7a59f7621b10a630c5407c964
sungjh <jinhyuk9714@gmail.com>
2025-07-06 01:56:47 +0900

    25.07.06 커밋 테스트

--- 1st_week/01_05_find_not_repeating_first_character.py
+++ 1st_week/01_05_find_not_repeating_first_character.py
@@ -25,3 +25,4 @@ result = find_not_repeating_first_character
 print("정답 = d 현재 풀이 값 =", result("abadabac"))
 print("정답 = c 현재 풀이 값 =", result("aabbcddd"))
 print("정답 =_ 현재 풀이 값 =", result("aaaaaaaa"))
+
