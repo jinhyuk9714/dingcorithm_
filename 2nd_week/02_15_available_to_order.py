dab17d16e114da2aa7c4e5b6e7631017299bc31b
sungjh <jinhyuk9714@gmail.com>
2025-07-10 02:44:00 +0900

    25.07.10 연결리스트, 이진탐색, Set 문제 풀이

---
+++ 2nd_week/02_15_available_to_order.py
@@ -0,0 +1,29 @@
+shop_menus = ["만두", "떡볶이", "오뎅", "사이다", "콜라"]
+shop_orders = ["오뎅", "콜라", "만두"]
+
+
+def is_available_to_order(menus, orders): # O(NlogN) + O(M) * O(logN)
+    menus.sort() # 메뉴의 길이가 N. O(NlogN)
+    for order in orders: # 오더의 길이가 M. O(M)
+        if not is_existing_target_order_binary(order, menus): # O(logN)
+            return False
+    return True
+
+def is_existing_target_order_binary(target, array):
+    cur_min = 0
+    cur_max = len(array) - 1
+    cur_guess = (cur_min + cur_max) // 2
+
+    while cur_min <= cur_max:
+        if array[cur_guess] == target:
+            return True
+        elif array[cur_guess] < target:
+            cur_min = cur_guess + 1
+        else:
+            cur_max = cur_guess - 1
+        cur_guess = (cur_min + cur_max) // 2
+
+    return False
+
+result = is_available_to_order(shop_menus, shop_orders)
+print(result)
