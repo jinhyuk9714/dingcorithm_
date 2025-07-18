9e9cd22f0eb701e60461797aa6c1a0482deac1f6
sungjh <jinhyuk9714@gmail.com>
2025-07-18 23:33:59 +0900

    25.07.18 힙 삭제

--- 4th_week/04_01_max_heap_insert.py
+++ 4th_week/04_01_max_heap_insert.py
@@ -16,9 +16,36 @@ class MaxHeap:
 
         return self.items
 
+    def delete(self):
+        self.items[1], self.items[-1] = self.items[-1], self.items[1]
+        prev_max = self.items.pop()
+        cur_index = 1
+
+        while cur_index <= len(self.items) - 1:
+            left_child_index = cur_index * 2
+            right_child_index = cur_index * 2 + 1
+            max_index = cur_index
+
+            if left_child_index <= len(self.items) - 1 and self.items[left_child_index] > self.items[max_index]:
+                max_index = left_child_index
+
+            if right_child_index <= len(self.items) - 1 and self.items[right_child_index] > self.items[max_index]:
+                max_index = right_child_index
+
+            if max_index == cur_index:
+                break
+
+            self.items[cur_index], self.items[max_index] = self.items[max_index], self.items[cur_index],
+            cur_index = max_index
+
+        return prev_max
+
+
 max_heap = MaxHeap()
 max_heap.insert(3)
 max_heap.insert(4)
 max_heap.insert(2)
 max_heap.insert(9)
 print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
+print(max_heap.delete())
+print(max_heap.items)
