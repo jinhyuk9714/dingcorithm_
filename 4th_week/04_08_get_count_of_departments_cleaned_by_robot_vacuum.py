4863ed3991284f5e978b6a21541a641398a4732e
sungjh <jinhyuk9714@gmail.com>
2025-07-21 08:35:58 +0900

    25.07.21 로봇 청소기

--- 4th_week/04_08_get_count_of_departments_cleaned_by_robot_vacuum.py
+++ 4th_week/04_08_get_count_of_departments_cleaned_by_robot_vacuum.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 current_r, current_c, current_d = 7, 4, 0
 current_room_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -15,11 +17,45 @@ current_room_map = [
 
 
 def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
-    return
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]  # 북 동 남 서 / 0 1 2 3
+
+    cleaned_count = 0
+
+    while True:
+        # 현재 위치 청소
+        if room_map[r][c] == 0:
+            room_map[r][c] = 2
+            cleaned_count += 1
+
+        cleaned = False
+        for _ in range(4):
+            # 왼쪽 방향 계산
+            d = (d + 3) % 4
+            nr, nc = r + dr[d], c + dc[d]
+
+            if room_map[nr][nc] == 0:
+                r, c = nr, nc
+                cleaned = True
+                break
+
+        if cleaned:
+            continue  # 청소 루프 다시
+
+        # 네 방향 모두 안됨, 후진 시도
+        back_d = (d + 2) % 4
+        br, bc = r + dr[back_d], c + dc[back_d]
+        if room_map[br][bc] == 1:
+            break
+        else:
+            r, c = br, bc
+
+    return cleaned_count
 
 
 # 57 가 출력되어야 합니다!
 print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
+
 current_room_map2 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
@@ -33,7 +69,8 @@ current_room_map2 = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
-print("정답 = 29 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,3,1,current_room_map2))
+print("정답 = 29 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6, 3, 1, current_room_map2))
+
 current_room_map3 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
@@ -47,7 +84,8 @@ current_room_map3 = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
-print("정답 = 33 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(7,4,1,current_room_map3))
+print("정답 = 33 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(7, 4, 1, current_room_map3))
+
 current_room_map4 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
@@ -61,4 +99,4 @@ current_room_map4 = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
-print("정답 = 25 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,2,0,current_room_map4))
+print("정답 = 25 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6, 2, 0, current_room_map4))
